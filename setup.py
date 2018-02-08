bdc38a2f2dd5991e96461f848e2322b1a59f9644
jamores <biomol@gmail.com>
2018-02-08 23:45:02 +0100

    wheel dist created OK

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='eth_scapy_someip',
-      version='1.0.0',
-      py_modules=[],
-      )
+setup(
+  name="eth_scapy_someip",
+  version="1.0.0",
+  author="Jose Amores",
+  description="Automotive Ethernet SOME/IP-SD Scapy protocol",
+  license="MIT",
+  py_modules=[],
+  packages=['eth_scapy_someip'],
+)
