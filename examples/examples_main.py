c26acec37d3bd18e647b1b4c42847caf335d434b
jamores <biomol@gmail.com>
2018-03-20 23:07:40 +0100

    SD example

--- examples/examples_main.py
+++ examples/examples_main.py
@@ -2,11 +2,13 @@ import unittest
 import HtmlTestRunner
 from ex_00_basics import ex_00_basics
 from ex_01_someip import ex_01_someip
+from ex_02_sd import ex_02_sd
 
 def suite():
   suite = unittest.TestSuite()
   suite.addTest(unittest.makeSuite(ex_00_basics))
   suite.addTest(unittest.makeSuite(ex_01_someip))
+  suite.addTest(unittest.makeSuite(ex_02_sd))
   return suite
 
 if __name__=='__main__':
