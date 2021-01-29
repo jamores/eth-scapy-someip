17a54a1ea14f4e6e4ebe24a39d2919c2e661e2b4
jamores <biomol@gmail.com>
2021-01-29 15:04:36 +0100

    someip_sd tests to pytest

--- tests/test_someip_sd.py
+++ tests/test_someip_sd.py
@@ -9,5 +9,21 @@ from eth_scapy_someip import eth_scapy_sd as sd
 
 HERE = os.path.dirname(os.path.realpath(__file__))
 
-def test_00():
-    pass
+def test_00_SOMEIPSD():
+    p_sd = sd.SD()
+    p_someip = p_sd.getSomeip()
+
+    # check SOME/IP-SD defaults
+    assert(binascii.hexlify(str(p_someip.msg_id)) == "ffff8100")
+    assert(p_someip.msg_type == someip.SOMEIP.TYPE_NOTIFICATION)
+
+    # length of SOME/IP-SD without entries nor options
+    p = p_someip/p_sd
+    assert(struct.unpack("!L",str(p)[4:8])[0] == 20)
+
+    # check SOME/IP-SD lengths (note : lengths calculated on package construction)
+    del(p)
+    p_sd.setEntryArray([sd.SDEntry_Service()])
+    p_sd.setOptionArray([sd.SDOption_IP4_EndPoint()])
+    p = p_someip/p_sd
+    assert(struct.unpack("!L",str(p)[4:8])[0] == 48)
