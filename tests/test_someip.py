1ff9b727662f3dd8344b62d0631d7aa39b5e95fc
Jose Amores <biomol@gmail.com>
2021-01-29 11:40:17 +0100

    pytest skeleton

---
+++ tests/test_someip.py
@@ -0,0 +1,12 @@
+import binascii
+import struct
+import sys
+import os
+import pytest
+
+from eth_scapy_someip import eth_scapy_someip as someip
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+
+def test_00():
+    pass
---
+++ tests/test_someip_sd.py
@@ -0,0 +1,13 @@
+import binascii
+import struct
+import sys
+import os
+import pytest
+
+from eth_scapy_someip import eth_scapy_someip as someip
+from eth_scapy_someip import eth_scapy_sd as sd
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+
+def test_00():
+    pass
